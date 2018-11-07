eb8e08d4a7f93e804a745c6f79a1d7229a34877f
JeaGeon <resistangce@gmail.com>
2018-11-07 17:14:04 +0900

    Update TTS/src/ServerMain.java

--- TTS/src/ServerMain.java
+++ TTS/src/ServerMain.java
@@ -1,4 +1,27 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerMain {
-
+	public static void main(String[] args) {
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(9001);
+			while (true) {
+				System.out.println("wait for client...");
+				Socket s = server.accept();
+				WorkingTread wt = new WorkingTread(s);
+				wt.run();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (server != null)
+				try {
+					server.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+	}
 }
