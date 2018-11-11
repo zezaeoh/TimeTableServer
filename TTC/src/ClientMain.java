572d9a13d2549c8d5dcdcded93e6ad2083f8ed93
lm040466 <lm040466@ajou.ac.kr>
2018-11-11 11:01:06 +0900

    Update TTC/src/ClientMain.java

--- TTC/src/ClientMain.java
+++ TTC/src/ClientMain.java
@@ -1,7 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.Buffer;
+import java.util.Scanner;
 
 public class ClientMain {
 	static final String ipAdress = "54.180.98.17";
@@ -9,14 +13,53 @@ public class ClientMain {
 	public static void main(String[] args) {
 		Socket s = null;
 		BufferedReader br = null;
+		PrintWriter pw = null;
+		String msg = null;
+		Scanner sc = new Scanner(System.in);
 		try {
 			System.out.println("Test to connect server...");
 			s = new Socket(ipAdress, 9001);
 			br = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF8"));
-			System.out.println(br.readLine());
-			System.out.println("Server connection closed");
-			System.out.println("hello");
+			pw = new PrintWriter(new OutputStreamWriter(s.getOutputStream(), "UTF8"));
+			ReadThread rt = new ReadThread(s, br);
+			rt.start();
+			while(true) {
+				msg = sc.nextLine();
+				if(msg != null) {
+					pw.println(msg);
+					if(msg.equals("종료") || msg.equals("접속종료"))
+						break;
+				}
+			}
 		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sc.close();
+			try {
+				s.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class ReadThread extends Thread {
+	Socket s = null;
+	BufferedReader br = null;
+	public ReadThread(Socket s, BufferedReader br) {
+		this.s = s;
+		this.br = br;
+	}
+	
+	public void run() {
+		try {
+			String input = null;
+			while(true) {
+				if((input = br.readLine()) != null)
+					System.out.println(input);
+			}
+		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
