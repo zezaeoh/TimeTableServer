38bbfb9210fea0b89cee632128571dddccf20e57
JeaGeon <resistangce@gmail.com>
2018-11-22 10:33:57 +0900

    connecting mysql connector to eclips project

--- TTS/src/WorkingTread.java
+++ TTS/src/WorkingTread.java
@@ -6,17 +6,17 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.LinkedList;
 
-public class WorkingTread extends Thread{
+public class WorkingTread extends Thread {
 	private ClientInfo client;
 	private PrintWriter pw;
 	private BufferedReader br;
 	private LinkedList<ClientInfo> users;
-	
+
 	public WorkingTread(Socket s, LinkedList<ClientInfo> users) {
 		this.users = users;
 		try {
 			pw = new PrintWriter(new OutputStreamWriter(s.getOutputStream(), "UTF8"));
-			br = new BufferedReader(new InputStreamReader(s.getInputStream(),"UTF8"));
+			br = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF8"));
 			client = new ClientInfo(s, br.readLine());
 			synchronized (users) {
 				users.add(client);
@@ -25,27 +25,29 @@ public class WorkingTread extends Thread{
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void run() {
 		String s = null;
 		System.out.println("Access: " + client.getCs().getInetAddress() + "@" + client.getId());
 		printMain();
-		while(true) {
+		while (true) {
 			pw.println(">> ");
 			pw.flush();
 			try {
 				s = br.readLine();
-				System.out.println("Client: " +  client.getId()  + " enterd msg: " + s);
-				if(s == null || s.isEmpty())
+				System.out.println("Client: " + client.getId() + " enterd msg: " + s);
+				if (s == null || s.isEmpty())
 					continue;
-				if(s.equals("종료") || s.equals("접속종료"))
+				if (s.equals("종료") || s.equals("접속종료"))
 					break;
-				else if(s.equals("동시접속자") || s.equals("동접자"))
+				else if (s.equals("동시접속자") || s.equals("동접자"))
 					checkConcurrentUsers();
 				else {
-					pw.println("이해하지 못하는 명령어 입니다!");
-					pw.flush();
+					if (!processMsg()) {
+						pw.println("이해하지 못하는 명령어 입니다!");
+						pw.flush();
+					}
 				}
 			} catch (Exception e) {
 				break;
@@ -64,12 +66,17 @@ public class WorkingTread extends Thread{
 		}
 	}
 
+	private boolean processMsg() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 	private void checkConcurrentUsers() {
 		int n;
 		synchronized (users) {
 			n = users.size();
 			pw.println("--현재 챗봇 접속자 수: " + n + "명");
-			for(ClientInfo s: users)
+			for (ClientInfo s : users)
 				pw.println("----ip@id: " + s.getCs().getInetAddress() + "@" + s.getId());
 			pw.flush();
 		}
